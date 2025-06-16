fb02156c66f0a7792dcb57c3736d8b34442108b2
jerinmt <jerinmt379@gmail.com>
2025-06-16 23:12:55 +0530

    Added the file creation and download feature

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let size=8;
 let color = '#000000';
 
 function drawline(Event) {
+    Event.target.style.backgroundImage = `none`;
     Event.target.style.opacity = `1`;
     Event.target.style.backgroundColor = `${color}`;
-    Event.target.style.color = `${color}`;
 }
 
 function createGrid() {
@@ -17,7 +17,6 @@ function createGrid() {
         divRow[i].classList.add("gridRows");
         for(let j=0;j<size;j++) {
             divSquare[j] = document.createElement("div");
-            divSquare[j].textContent = '.';
             divRow[i].appendChild(divSquare[j]);
             divSquare[j].classList.add("gridBoxes");
             divSquare[j].addEventListener('click', (Event) => {
@@ -33,15 +32,14 @@ function createGrid() {
                 }
                 else {
                     x = 1;
+                    Event.target.style.backgroundImage = `none`;
                     Event.target.style.backgroundColor = `${color}`;
-                    Event.target.style.color = `${color}`;
                 }
                 Event.target.style.opacity = `${x}`;
             }, false);
             divSquare[j].addEventListener('contextmenu', (Event) => {
                 Event.preventDefault();
-                Event.target.style.color = `black`;
-                Event.target.style.backgroundColor = `white`;
+                Event.target.style.backgroundImage = `radial-gradient(black, white)`;
                 Event.target.style.opacity = 0.9;
             }, false);
         }
@@ -65,7 +63,7 @@ function setResolution () {
     }
     createGrid();
 }
-document.querySelector("button").addEventListener('click', setResolution);
+document.querySelector(".resetButton").addEventListener('click', setResolution);
 document.querySelector("select").addEventListener('change', setResolution);
 document.querySelector("input").addEventListener('change', () => {
     color = document.querySelector('input').value;
@@ -76,3 +74,52 @@ function removeGrid() {
         container.removeChild(divRow[i]);
     }
 }
+
+document.querySelector("dialog").showModal();
+document.querySelector(".OKButton").addEventListener('click', () => {
+    document.querySelector("dialog").close();
+}, false);
+
+function downloadFile(content, filename, contentType) {
+    const blob = new Blob([content], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
+
+document.querySelector(".downloadButton").addEventListener('click', () => {
+    let fileContent = `<svg width = "${size}" height = "${size}" xmlns = "http://www.w3.org/2000/svg">`;
+    //write the pixels
+    const grid = document.querySelectorAll('.gridBoxes');
+    let start, end, currentColor, currentOpacity;
+    for(let i=0; i < size; i++) {
+        for(let j=0; j < size; j++) {
+            let k = i * size + j;
+            if(grid[k].style.opacity == '1' || grid[k].style.opacity == '0.6' || grid[k].style.opacity == '0.2') {
+                currentColor = grid[k].style.backgroundColor;
+                currentOpacity = grid[k].style.opacity;
+                start = j;
+                while (currentOpacity == grid[i * size + j + 1].style.opacity && 
+                    currentColor == grid[i * size + j + 1].style.backgroundColor && (j != (size - 1))) {
+                    j += 1;
+                    k += 1;
+                }
+                end = j;
+                fileContent += `<line x1="${start}" y1="${i}" x2="${end + 1}" y2="${i}" stroke="${currentColor}" opacity="${currentOpacity}"/>` ;
+            }
+        }
+    }
+    fileContent += '</svg>';
+    console.log(fileContent);
+    const fileName = "PicArt.svg";
+    downloadFile(fileContent, fileName, "text/plain");
+});
+
+document.querySelector(".donateButton").addEventListener('click', () => {
+    console.log("Donating money");
+});
